2dff8ef16fc51110c26791db63ec002a58c45af3
geun <minkeon44@naver.com>
2024-09-09 21:28:39 +0900

    Product List,Insert

--- src/main/java/com/example/coffeeshop/product/repository/ProductRepository.java
+++ src/main/java/com/example/coffeeshop/product/repository/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.example.coffeeshop.product.repository;
 
-public interface ProductRepository {
+import com.example.coffeeshop.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
 }
