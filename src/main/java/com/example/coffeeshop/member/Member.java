cf11dc8fa2a86e491dc8c8ca6eb55071db5b8288
천영상 <chuns7740@gmail.com>
2024-09-09 09:41:30 +0900

    feat : Member 엔티티 개발#4

---
+++ src/main/java/com/example/coffeeshop/member/Member.java
@@ -0,0 +1,44 @@
+package com.example.coffeeshop.member;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "members", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false, length = 20)
+    private String email;
+
+    @Column(length = 255)
+    private String address;
+
+    @Column(length = 255)
+    private String postcode;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public Member(String email, String address, String postcode) {
+        this.email = email;
+        this.address = address;
+        this.postcode = postcode;
+    }
+}
---
+++ src/main/java/com/example/coffeeshop/member/MemberRepository.java
@@ -0,0 +1,2 @@
+package com.example.coffeeshop.member;public class MemberRepository {
+}
