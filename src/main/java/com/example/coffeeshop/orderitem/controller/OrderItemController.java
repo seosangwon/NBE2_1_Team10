2d0d7f34f485aaa4c8b7801f972054b023b6ee71
Preta3418 <jason123qwe@gmail.com>
2024-09-10 16:04:05 +0900

    Controller 개발

--- src/main/java/com/example/coffeeshop/orderitem/controller/OrderItemController.java
+++ src/main/java/com/example/coffeeshop/orderitem/controller/OrderItemController.java
@@ -1,4 +1,40 @@
 package com.example.coffeeshop.orderitem.controller;
 
+import com.example.coffeeshop.orderitem.domain.OrderItem;
+import com.example.coffeeshop.orderitem.service.OrderItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orderitems")
 public class OrderItemController {
+
+    @Autowired
+    private OrderItemService orderItemService;
+
+    // View
+    @GetMapping
+    public List<OrderItem> getAllOrderItems() {
+        return orderItemService.getAllOrderItems();
+    }
+
+    // Create
+    @PostMapping
+    public OrderItem createOrderItem(@RequestBody OrderItem orderItem) {
+        return orderItemService.saveOrderItem(orderItem);
+    }
+
+    // View
+    @GetMapping("/{id}")
+    public OrderItem getOrderItemById(@PathVariable Long id) {
+        return orderItemService.getOrderItemById(id);
+    }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    public void deleteOrderItem(@PathVariable Long id) {
+        orderItemService.deleteOrderItem(id);
+    }
 }
