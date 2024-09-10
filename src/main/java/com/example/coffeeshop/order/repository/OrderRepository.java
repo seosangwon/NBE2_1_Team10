016af9d65af1c654c7a09dc595888c7a3252fa93
Gongjjin <gjyj5395@naver.com>
2024-09-11 00:51:33 +0900

    feat: ID로 주문 조회 및, 주문 상태에 따른 주문 조회 메서드 작성

--- src/main/java/com/example/coffeeshop/order/repository/OrderRepository.java
+++ src/main/java/com/example/coffeeshop/order/repository/OrderRepository.java
@@ -1,12 +1,17 @@
 package com.example.coffeeshop.order.repository;
 
 import com.example.coffeeshop.order.domain.Order;
+import com.example.coffeeshop.order.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    // ID로 주문 조회
     Optional<Order> findByOrderId(Long id);
+    // 주문 상태에 따른 주문 조회
+    List<Order> findByOrderStatus(OrderStatus orderStatus);
 }
