624e5dc9fdbbe082d48040ffd195a8c2b5f577f1
Gongjjin <gjyj5395@naver.com>
2024-09-11 00:56:54 +0900

    feat: 컴파일 에러로 인한 임시 Repository 작성

--- src/main/java/com/example/coffeeshop/orderitem/repository/OrderItemRepository.java
+++ src/main/java/com/example/coffeeshop/orderitem/repository/OrderItemRepository.java
@@ -1,4 +1,7 @@
 package com.example.coffeeshop.orderitem.repository;
 
-public interface OrderItemRepository {
+import com.example.coffeeshop.orderitem.domain.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
