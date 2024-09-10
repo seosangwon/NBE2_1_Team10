1730c18a6ea316dac217a59233a790b09f6a569f
Gongjjin <gjyj5395@naver.com>
2024-09-11 03:13:18 +0900

    feat: 주문 조회를 주문 상태에서 회원의 email로 변경

--- src/main/java/com/example/coffeeshop/order/repository/OrderRepository.java
+++ src/main/java/com/example/coffeeshop/order/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.example.coffeeshop.order.repository;
 
+import com.example.coffeeshop.member.domain.Member;
 import com.example.coffeeshop.order.domain.Order;
 import com.example.coffeeshop.order.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +13,6 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     // ID로 주문 조회
     Optional<Order> findByOrderId(Long id);
-    // 주문 상태에 따른 주문 조회
-    List<Order> findByOrderStatus(OrderStatus orderStatus);
+    // 주문 회원 email로 조회
+    List<Order> findByMember(Member member);
 }
