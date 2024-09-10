4872f95b47ecab79472da5a4f4f1059cb0b9b4d2
Preta3418 <jason123qwe@gmail.com>
2024-09-10 17:29:28 +0900

    RequstMapping pdf 대로 바꿈

--- src/main/java/com/example/coffeeshop/orderitem/controller/OrderItemController.java
+++ src/main/java/com/example/coffeeshop/orderitem/controller/OrderItemController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/orderitems")
+@RequestMapping("/api/v1/orderitems")
 public class OrderItemController {
 
     @Autowired
