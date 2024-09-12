dd826e7975ff00f1122ac9b9e991ea0b8e89aa6b
Gongjjin <gjyj5395@naver.com>
2024-09-12 11:26:01 +0900

    OrderDto에 제약사항 추가

--- src/main/java/com/example/coffeeshop/order/dto/OrderDto.java
+++ src/main/java/com/example/coffeeshop/order/dto/OrderDto.java
@@ -1,7 +1,7 @@
 package com.example.coffeeshop.order.dto;
 
-import com.example.coffeeshop.order.domain.Order;
 import com.example.coffeeshop.order.domain.OrderStatus;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,30 +10,52 @@ import java.util.Map;
 public class OrderDto {
     @Data
     public static class CreateRequestDto{
-       private Map<Long, Integer> productQuantityMap;
+        @NotNull(message = "물품과 수량은 공백일 수 없습니다")
+        @NotEmpty(message = "물품과 수량은 둘 다 1개 이상이어야합니다")
+        private Map<Long, Integer> productQuantityMap;
+
+        @NotBlank(message = "우편번호는 공백이 허용되지 않습니다")
         private String postcode;
+
+        @NotBlank(message = "주소는 공백이 허용되지 않습니다")
         private String address;
+
+        @NotNull(message = "Member ID 공백이 허용되지 않습니다")
         private Long memberId;
     }
     @Data
     public static class CreateResponseDto {
         private Long orderId;
+
+        @NotBlank(message = "주소는 공백이 허용되지 않습니다")
         private String address;
+
+        @NotBlank(message = "우편번호는 공백이 허용되지 않습니다")
         private String postcode;
+
         private LocalDateTime createdAt;
+
+        @Min(value = 0, message = "최소 주문금액은 0원 이상이어야 합니다")
         private int totalPrice;
+
     }
     @Data
     public static class FindResponseDto{
+        @NotNull(message = "Order Id는 공백이 허용되지 않습니다")
         private Long orderId;
+
+        @NotBlank(message = "주소는 공백이 허용되지 않습니다")
         private String address;
+
+        @NotBlank(message = "우편번호는 공백이 허용되지 않습니다")
         private String postcode;
+
         private LocalDateTime createdAt;
+
+        @NotNull(message = "주문 상태는 공백이 허용되지 않습니다")
         private OrderStatus orderStatus;
+
+        @NotNull(message = "Member ID 공백이 허용되지 않습니다")
         private Long memberId;
-    }
-    @Data
-    public static class OrderStatusDto{
-        private OrderStatus orderStatus;
     }
 }
