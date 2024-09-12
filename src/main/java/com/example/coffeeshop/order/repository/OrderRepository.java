86c81657bb02b5a8b32662f69f4ee243c6af19f8
Gongjjin <gjyj5395@naver.com>
2024-09-12 09:46:44 +0900

    refactor : 주문 조회 시 email에서 memberid로 변경

--- src/main/java/com/example/coffeeshop/order/repository/OrderRepository.java
+++ src/main/java/com/example/coffeeshop/order/repository/OrderRepository.java
@@ -2,15 +2,13 @@ package com.example.coffeeshop.order.repository;
 
 import com.example.coffeeshop.member.domain.Member;
 import com.example.coffeeshop.order.domain.Order;
-import com.example.coffeeshop.order.domain.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    // 주문 회원 email로 조회
-    List<Order> findByMemberEmail(String email);
+    // 주문 id로 조회
+    List<Order> findOrderByMember(Member member);
 }
